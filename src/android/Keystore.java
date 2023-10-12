786fcce2407cd30dcbb3f3cbe7689409902cad0c
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-10-12 16:04:16 +0300

    create keypair

--- src/android/Keystore.java
+++ src/android/Keystore.java
@@ -4,6 +4,15 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.security.KeyPairGenerator;
+import java.security.NoSuchProviderException;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.Calendar;
+
+import android.security.keystore.KeyProperties;
+import android.security.keystore.KeyGenParameterSpec;
+
 public class KeyStore extends CordovaPlugin {
 
     @Override
@@ -22,5 +31,21 @@ public class KeyStore extends CordovaPlugin {
             return false;
 
         }
+    }
+
+    private void createKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+      KeyPairGenerator kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+
+      Calendar start = Calendar.getInstance();
+      KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
+              "directdemocracyAppKey",
+              KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
+              .setDigests(KeyProperties.DIGEST_SHA512)
+              .setKeyValidityStart(start.getTime())
+              .build();
+
+      kpg.initialize(spec);
+
+      kpg.generateKeyPair();
     }
 }
