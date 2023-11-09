1c8b7f84fd435fffd30521bb126c616198b90ebf
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-11-09 12:55:50 +0100

    Progress with iOS2

--- src/ios/Keystore.m
+++ src/ios/Keystore.m
@@ -4,12 +4,35 @@
 @implementation Keystore
 
 - (void)createKeyPair:(CDVInvokedUrlCommand*)command {
+  NSString* alias = [command.arguments objectAtIndex:0];
+  NSData* tag = [alias dataUsingEncoding:NSUTF8StringEncoding];
+  NSDictionary* attributes = @{
+    (id)kSecAttrKeyType: (id)kSecAttrKeyTypeRSA,
+    (id)kSecAttrKeySizeInBits: @2048,
+    (id)kSecPrivateKeyAttrs: @{(id)kSecAttrIsPermanent: @YES, (id)kSecAttrApplicationTag: tag}
+  };
   CDVPluginResult* pluginResult = nil;
-  NSString* echo = [command.arguments objectAtIndex:0];
-  if (echo != nil && [echo length] > 0) {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+  CFErrorRef error = NULL;
+  SecKeyRef privateKey = SecKeyCreateRandomKey((__bridge CFDictionaryRef)attributes, &error);
+  if (!privateKey) {
+    NSError *err = CFBridgingRelease(error);
+    NSString *message = [NSString stringWithFormat:@"Cannot generate key pair: @", [err localizedDescription]];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
   } else {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    SecKeyRef publicKey = SecKeyCopyPublicKey(privateKey);
+    if (!publicKey) {
+      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Cannot export public key"];
+    } else {
+      NSData* keyData = (NSData*)CFBridgingRelease(SecKeyCopyExternalRepresentation(publicKey, &error));
+      if (!keyData) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Failed to export public key"];
+      } else {
+        NSString *pem = [keyData base64EncodedStringWithOptions:0];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:pem];
+      }
+    }
+    // [myString release];
+      CFRelease(privateKey);
   }
   [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
